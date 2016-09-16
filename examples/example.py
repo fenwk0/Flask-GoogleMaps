04d8134a15a3f7efed3dcef0177c7dd16f55897d
fenwk0 <karl.fenwick@gmail.com>
2016-09-16 10:40:39 +0100

    Isolated relatively simple example of python geo-locate

--- examples/example.py
+++ examples/example.py
@@ -12,6 +12,47 @@ app.config['GOOGLEMAPS_KEY'] = "AIzaSyAZzeHhs-8JZ7i18MjFuM35dJHq70n3Hx4"
 # you can also pass key here
 GoogleMaps(app, key="AIzaSyAZzeHhs-8JZ7i18MjFuM35dJHq70n3Hx4")
 
+@app.route("/geo-locate")
+def geolocate():
+
+    trdmap = Map(
+        identifier="trdmap",
+        varname="trdmap",
+        lat=37.4419,
+        lng=-122.1419,
+        markers=[
+            {
+                'icon': icons.alpha.B,
+                'lat': 37.4419,
+                'lng': -122.1419,
+                'infobox': "Hello I am <b style='color:green;'>GREEN</b>!"
+            },
+            {
+                'icon': icons.dots.blue,
+                'lat': 37.4300,
+                'lng': -122.1400,
+                'infobox': "Hello I am <b style='color:blue;'>BLUE</b>!"
+            },
+            {
+                'icon': '//maps.google.com/mapfiles/ms/icons/yellow-dot.png',
+                'lat': 37.4500,
+                'lng': -122.1350,
+                'infobox': (
+                    "Hello I am <b style='color:#ffcc00;'>YELLOW</b>!"
+                    "<h2>It is HTML title</h2>"
+                    "<img src='//placehold.it/50'>"
+                    "<br>Images allowed!"
+                )
+            }
+        ]
+    )
+
+    return render_template(
+        'geo-locate.html',
+        trdmap=trdmap
+
+    )
+
 
 @app.route("/")
 def mapview():
